70cc4cfe90b3b1a51d9aa62c948fd49cfb62abaf
Zheng Ye <87119797+ZeffJsdan@users.noreply.github.com>
2022-02-04 00:07:30 +0800

    Add Main Page

--- app/src/main/java/com/example/andeca1/MainActivity.java
+++ app/src/main/java/com/example/andeca1/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.andeca1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CardView budgetCardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        budgetCardView = findViewById(R.id.budgetCardView);
+    }
+
+    public void onClickBudget(View v){
+        Intent intent = new Intent(MainActivity.this,BudgetActivity.class);
+        startActivity(intent);
     }
 }
