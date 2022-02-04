bdb09db2d6e9e66c72d69207403b69ce4c9bc5ea
Zheng Ye <87119797+ZeffJsdan@users.noreply.github.com>
2022-02-04 10:11:49 +0800

    Added Weekly Features

--- app/src/main/java/com/example/andeca1/MainActivity.java
+++ app/src/main/java/com/example/andeca1/MainActivity.java
@@ -10,14 +10,20 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private CardView budgetCardView,dailyCardView;
-    private ImageView weekBtnImageView,budgetBtnImageView,dailyBtnImageView;
+    private ImageView weekBtnImageView,budgetBtnImageView,dailyBtnImageView, monthBtnImageView,
+            todayAnalyticImageView,weekAnalyticImageView,monthAnalyticImageView,statisticCardView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         budgetBtnImageView = findViewById(R.id.budgetBtnImageView);
         dailyBtnImageView = findViewById(R.id.dailyBtnImageView);
+        monthBtnImageView = findViewById(R.id.monthBtnImageView);
+        todayAnalyticImageView = findViewById(R.id.todayAnalyticImageView);
+        weekAnalyticImageView = findViewById(R.id.weekAnalyticImageView);
+        monthAnalyticImageView = findViewById(R.id.monthAnalyticImageView);
+        statisticCardView = requireViewById(R.id.statisticCardView);
+
     }
 
     public void onClickBudget(View v){
@@ -30,6 +36,20 @@ public class MainActivity extends AppCompatActivity {
     }
     public void onClickWeekly(View v){
         Intent intent = new Intent(MainActivity.this,WeeklyActivity.class);
+        intent.putExtra("type","week");
         startActivity(intent);
     }
+
+    public void onClickMonthly(View v){
+        Intent intent = new Intent(MainActivity.this,WeeklyActivity.class);
+        intent.putExtra("type","month");
+        startActivity(intent);
+    }
+    public void onClickAnalytic(View v){
+        Intent intent = new Intent(MainActivity.this,MultiAnalyticsActivity.class);
+
+        startActivity(intent);
+    }
+
+
 }
